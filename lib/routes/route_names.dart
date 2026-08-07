c8cecfecdcd9857be24b962f7d97d84e4e721e7a
amarjeet-byte <amarjeetds12@gmail.com>
2026-08-07 20:04:08 +0530

    Routes Name

---
+++ lib/routes/route_names.dart
@@ -0,0 +1,11 @@
+class RouteNames {
+  RouteNames._();
+
+  static const dashboard = '/';
+  static const urlDetection = '/url-detection';
+  static const qrDetection = '/qr-detection';
+  static const imageDetection = '/image-detection';
+  static const history = '/history';
+  static const profile = '/profile';
+  static const settings = '/settings';
+}
